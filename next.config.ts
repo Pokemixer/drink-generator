4d392abeecb15e232e0d43d33f4e11d470578ccc
ni-jessica <jessica_ni@brown.edu>
2025-05-08 15:17:20 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,12 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/**')],
+    loader: "akamai",
+    path: "",
   },
   output: 'export',
+  basePath: '/drink-generator',
+  assetPrefix: "/drink-generator/",
 };
 
 export default withFlowbiteReact(nextConfig);
