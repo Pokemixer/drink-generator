63260a7f4f9a7130333023e89e7a863a25407ba0
ni-jessica <jessica_ni@brown.edu>
2025-05-08 15:06:25 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/**'), new URL('https://img.apileague.com/recipes/**'), new URL("https://thespicyapron.com/**"), new URL("https://opendrinks.io/img/**"), new URL("https://www.thecocktaildb.com/images/media/**")],
-  }
+    remotePatterns: [new URL('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/**')],
+  },
+  output: 'export',
 };
 
 export default withFlowbiteReact(nextConfig);
